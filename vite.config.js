4f2ed289caa2d10bb5279bf1dd192ab624ef9663
Pavel Tomilov <pasha.tomiloff@yandex.ru>
2026-05-04 13:59:00 +0300

    update2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
+// vite.config.js
 import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "./",
   build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    emptyOutDir: true,
+    outDir: "public",
   },
-  publicDir: "public",
 });
